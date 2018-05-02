8367a90bbbb10e8e20a9d18dee7dadbecfb0d8db
ogo-oluwa jesutomi olasubulumi <tomilashy@yahoo.com>
2018-05-02 04:19:52 -0400

    prompt user to input

--- helloworld.py
+++ helloworld.py
@@ -12,4 +12,18 @@ fruit = "lemon";
 ripeness = "rotten";
 
 print("the {} is {}".format(fruit,ripeness));
-print("Hello world");
+print("Hello"+" world");
+print("Lol"[0],fruit[4])
+print(fruit[-4])
+
+#comment
+decimal= 12.35342;
+integer = int(decimal);
+print(integer)
+#mileage converter
+
+print("how many kilomerters did you cycle/run today?")
+kms=float(input())
+miles= round(kms/1.609,5) #round miles to 5 decimal places
+print(f"you ran {miles} miles ")
+
