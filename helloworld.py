050580ea12bfef6b13ffe4d790a53f1d5da106b4
ogo-oluwa jesutomi olasubulumi <tomilashy@yahoo.com>
2018-05-01 23:03:42 -0400

    learning python

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+'''
+Created on Mar 14, 2018
+ogo-oluwa jesutomi olasubulumi <tomilashy@yahoo.com>
+@author: ogo-oluwa.olasubulumi
+'''
+print("Enter an interger");
+
+fruit = "banana";
+ripeness = "unripe";
+print(f"the {fruit} is {ripeness}");
+fruit = "lemon";
+ripeness = "rotten";
+
+print("the {} is {}".format(fruit,ripeness));
+print("Hello world");
